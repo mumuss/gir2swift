c21afc7979dddd68335de415c74819e9a136f116
mumuss <michel.musslin@gmail.com>
2019-10-03 17:09:37 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let pkgName = "gir2swift"
 
 let package = Package(
     name: pkgName,
-    dependencies: [ .package(url: "https://github.com/rhx/SwiftLibXML.git", .branch("master")) ],
+    dependencies: [ .package(url: "https://github.com/mumuss/SwiftLibXML.git", .branch("master")) ],
     targets: [
         .target(name: pkgName, dependencies: ["SwiftLibXML"]),
         .testTarget(name: "\(pkgName)Tests", dependencies: [.byNameItem(name: pkgName)]),
